b8c5c9e6690389f4c94949a209fd0a6ec46d9fe0
Universidad de Sevilla <proyectodron2020@gmail.com>
2021-03-03 14:01:27 +0100

    Actualización del script de sensores
    Se ha programado para su compatibilidad para el Smart water y el SW IONS y además se ha añadido un campo para registrar la fecha de las muestras

--- Sensores/prueba_basedatos1.py
+++ Sensores/prueba_basedatos1.py
@@ -1,26 +1,28 @@
-#importar librería de sqlite
+#importar librer1a de sqlite
 import sqlite3
 
 #nos conectamos con la base de datos llamada prueba1.db
 #si no existe crea una con ese nombre en el mismo directorio
 conectar=sqlite3.connect("prueba1.db")
 
-#Creamos un cursor que nos permitirá interactuar con la base de datos
+#Creamos un cursor que nos permitira interactuar con la base de datos
 cursor=conectar.cursor()
 
 #ahora creamos la tabla con los datos que guardaremos
 cursor.execute("""CREATE TABLE sensor(
                 ID text,
+		SAMPLE_NUM real,
                 BAT real,
                 TEMP real,
                 PH real,
                 DO real,
                 COND real,
-                ORP real
+                ORP real,
+                DATE text
                 )""")
 
 #el siguiente comando "commit()" guarda la tabla creada
 conectar.commit()
 
-#cerramos la conexión con la base de datos
+#cerramos la conexion con la base de datos
 conectar.close()
